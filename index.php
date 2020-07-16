79eb444167d35fdae7085695c2028a0536e0cf13
Abdo <abdo@abdo.com>
2020-07-16 14:03:33 +0200

    css finished

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Tracks Task</title>
+
+	<link rel="stylesheet" href="layout/css/style.css">
+</head>
+<body>
+
+	<section class="main overlay flex">
+		<div class="left column-center">
+			<div>
+				<h1>WELCOME TO OUR AUDIO</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas distinctio optio aspernatur, quas vero, ipsam. Saepe vel eum, repellendus temporibus?</p>
+			</div>
+		</div>
+		
+		<div class="right">
+			<div class="tracks-container column-center">
+				<div class="tracks">
+					<h2 class="tracks-heading">CHOOSE YOUR FAVOURITE TRACK</h2>
+	
+					<div class="track flex">
+						<div class="track-num column-center">
+							<div class="num-circle overlay"><span>1</span></div>
+						</div>
+
+						<div class="track-info flex">
+							<div class="column-center">
+								<h3 class="track-name">Hello World</h3>
+								<div class="track-artist">Eminem</div>
+							</div>
+
+							<div class="track-length column-center">
+								<span>3:34</span>
+							</div>
+						</div>
+
+						<div class="track-download column-center">
+							<button class="download-btn">Download</button>
+						</div>
+					</div>
+
+					<div class="track flex">
+						<div class="track-num column-center">
+							<div class="num-circle overlay"><span>2</span></div>
+						</div>
+
+						<div class="track-info flex">
+							<div class="column-center">
+								<h3 class="track-name">Hello World</h3>
+								<div class="track-artist">Eminem</div>
+							</div>
+
+							<div class="track-length column-center">
+								<span>3:34</span>
+							</div>
+						</div>
+
+						<div class="track-download column-center">
+							<button class="download-btn">Download</button>
+						</div>
+					</div>
+
+					<div class="track flex">
+						<div class="track-num column-center">
+							<div class="num-circle overlay"><span>3</span></div>
+						</div>
+
+						<div class="track-info flex">
+							<div class="column-center">
+								<h3 class="track-name">Hello World</h3>
+								<div class="track-artist">Eminem</div>
+							</div>
+
+							<div class="track-length column-center">
+								<span>3:34</span>
+							</div>
+						</div>
+
+						<div class="track-download column-center">
+							<button class="download-btn">Download</button>
+						</div>
+					</div>
+
+					<div class="track flex">
+						<div class="track-num column-center">
+							<div class="num-circle overlay"><span>4</span></div>
+						</div>
+
+						<div class="track-info flex">
+							<div class="column-center">
+								<h3 class="track-name">Hello World</h3>
+								<div class="track-artist">Eminem</div>
+							</div>
+
+							<div class="track-length column-center">
+								<span>3:34</span>
+							</div>
+						</div>
+
+						<div class="track-download column-center">
+							<button class="download-btn">Download</button>
+						</div>
+					</div>
+
+					<div class="track flex">
+						<div class="track-num column-center">
+							<div class="num-circle overlay"><span>5</span></div>
+						</div>
+
+						<div class="track-info flex">
+							<div class="column-center">
+								<h3 class="track-name">Hello World</h3>
+								<div class="track-artist">Eminem</div>
+							</div>
+
+							<div class="track-length column-center">
+								<span>3:34</span>
+							</div>
+						</div>
+
+						<div class="track-download column-center">
+							<button class="download-btn">Download</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+</body>
+</html>
